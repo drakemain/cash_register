db5aae7d025baa282e4dd9a940274261777e9886
drakemain <demain1990@gmail.com>
2015-09-09 22:13:44 -0700

    added very basic users system

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var reg = require('./assets/js/register.js');
 var merch = require('./assets/js/coffeeShop.js')
+var database = {};
 
 app = express();
 
@@ -9,19 +10,79 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/', function (req, res) {
+  var output =  "<html>"+
+                  "<head>" +
+                    "<title>Login</title>" +
+                  "</head>" +
+                  "<body>" +
+                    "<form action='/login' method='post'>" +
+                      "<p>" +
+                        "Who are you?<br>"+
+                        "<input type='text' name='userID' value='usr'>" +
+                        "<input type='submit' value='submit'>" +
+                      "</p>" +
+                    "</form>" +
+                  "</body>" +
+                "</html>";
+  res.send(output);
+});
+
+app.get('/register/:user', function (req, res) {
+  var userID = req.params.user;
+  var operator = database[userID];
+
+  if (!database[userID]) {
+    res.redirect('/');
+  }
+
   var output = "<html><head><title>Cash Register</title></head><body>";
-  output += "<fieldset>" + reg.register.getFormatTotals() + "</fieldset>";
+  output += "<h2>Operator: " + operator.user + "</h2>";
+  output += "<fieldset>" + formatTotals(userID) + "</fieldset>";
   output += "<p>"
-  output += formatMenu();
-  output += "<input type='submit' value='Submit'>"
-  output += "<input type='submit' value='Reset' formaction='/reset'>"
+  output += formatMenu(userID);
+  output += "<input type='submit' value='Submit'>";
+  output += "<input type='submit' value='Reset' formaction='/reset/" + userID +"'>";
+  output += "<a href='/'>Logout</a>";
   output += "</form></p><p><a href='https://github.com/drakemain/cash_register/tree/node-app'>" + 
     "Source</a></body></html>";
 
   res.send(output);
 });
 
-app.post('/form-handler', function(req, res) {
+app.get('/showUserData/:user', function(req, res) {
+  if (database[req.params.user]){
+    res.send(database[req.params.user]);
+  } else {
+    res.send("No data exists for user " + req.params.user + ".");
+  }
+});
+
+app.get('/showDB', function(req, res) {
+  res.send(database);
+});
+
+app.post('/login', function(req, res) {
+  var userID = req.body.userID;
+  //var redirect = "/showUserData/" + userID;
+
+  if (!database[userID]) {
+    database[userID] = {
+      "user": userID,
+      "subTotal": 0,
+      "tax": 0,
+      "total": 0
+    };
+    console.log("New user " + userID + " created.");
+  } else {
+    console.log("Returning user " + userID + " logged in.");
+  }
+
+  res.redirect('/register/' + userID);
+  //res.send("Data submitted<br><a href='/'>Back</a>");
+});
+
+app.post('/form-handler/:user', function(req, res) {
+  
   var selection = JSON.parse(req.body.item);
 
   if (selection.size === "na") {
@@ -30,22 +91,28 @@ app.post('/form-handler', function(req, res) {
 
   console.log(selection)
 
-  reg.register.scanItem(merch.items[selection.item], selection.size, 1);
+  var scannedItem = reg.register.scanItem(merch.items[selection.item], selection.size, 1);
 
-  res.redirect('/');
+  console.log(scannedItem);
+
+  database[req.params.user].subTotal += scannedItem.subTotal;
+  database[req.params.user].tax += scannedItem.tax;
+  database[req.params.user].total += scannedItem.total;
+
+  res.redirect('/register/' + req.params.user);
 });
 
-app.post('/reset', function(req, res) {
-  reg.register.subTotal = 0;
-  reg.register.tax = 0;
-  reg.register.total = 0;
+app.post('/reset/:user', function(req, res) {
+  database[req.params.user].subTotal = 0;
+  database[req.params.user].tax = 0;
+  database[req.params.user].total = 0;
 
-  res.redirect('/');
+  res.redirect('/register/' + req.params.user);
 });
 
-var formatMenu = function() {
+var formatMenu = function(userID) {
   var i = 1;
-  var output = "<form action='/form-handler' method='post'>";
+  var output = "<form action='/form-handler/" + userID + "' method='post'>";
 
   for(var item in merch.items) {
     if (merch.items[item].sizes) {
@@ -63,6 +130,18 @@ var formatMenu = function() {
     }
   }
   return output;
+}
+
+var formatTotals = function(userID) {
+  formattedTotals = "Subtotal: " + database[userID].subTotal.toFixed(2) + "<br>";
+  formattedTotals += "Tax: " + database[userID].tax.toFixed(2) + " @ " + (reg.register.taxRate*100)  + "%<br>";
+  formattedTotals += "Total: " + database[userID].total.toFixed(2);
+
+  console.log("Subtotal: " + database[userID].subTotal.toFixed(2));
+  console.log("Tax: " + database[userID].tax.toFixed(2) + " @ " + (reg.register.taxRate*100)  + "%");
+  console.log("Total: " + database[userID].total.toFixed(2));
+
+  return formattedTotals;
 }
 
 app.listen(3000);
