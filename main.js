4ea077e96070d24eec75917aa73c6b43a3c4f88a
drakemain <demain1990@gmail.com>
2015-09-16 21:39:39 -0700

    housekeeping

--- main.js
+++ main.js
@@ -10,14 +10,6 @@ app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-/*app.get('/hbtest', function(req, res) {
-  res.render('hbtest', {
-    title: "test"
-  });
-
-  console.log('test');
-});*/
-
 var database;
 
 app.use(bodyParser.json()); 
@@ -31,40 +23,16 @@ app.get('/', function (req, res) {
 
 app.get('/register/:user', function (req, res) {
   var userID = req.params.user;
-  var operator = database[userID];
 
-  if (!operator) {
+  if (!database[userID]) {
     res.redirect('/');
   }
-
-  console.log(merch.items.coffee);
 
   res.render('register', {
     title: "Cash Register",
     operator: database[req.params.user],
     menu: merch.items
   });
-  /*res.render('register', (function () {
-    return {
-      title: "Cash Register",
-      operator: operator,
-    };
-  })());*/
-
-
-  /*
-  var output = "<html><head><title>Cash Register</title></head><body>";
-  output += "<h2>Operator: " + operator.user + "</h2>";
-  output += "<fieldset>" + formatTotals(userID) + "</fieldset>";
-  output += "<p>"
-  output += formatMenu(userID);
-  output += "<input type='submit' value='Submit'>";
-  output += "<input type='submit' value='Reset' formaction='/reset/" + userID +"'>";
-  output += "<a href='/'>Logout</a>";
-  output += "</form></p><p><a href='https://github.com/drakemain/cash_register/tree/node-app'>" + 
-    "Source</a></body></html>";
-
-  res.send(output);*/
 });
 
 app.get('/showUserData/:user', function(req, res) {
@@ -93,13 +61,12 @@ app.post('/login', function(req, res) {
 
     writeDB();
 
-    console.log("New user " + userID + " created.");
+    console.log("New user " + userID + " created.\n");
   } else {
-    console.log("Returning user " + userID + " logged in.");
+    console.log("Returning user " + userID + " logged in.\n");
   }
 
   res.redirect('/register/' + userID);
-  //res.send("Data submitted<br><a href='/'>Back</a>");
 });
 
 app.post('/form-handler/:user', function(req, res) {
@@ -133,40 +100,6 @@ app.post('/reset/:user', function(req, res) {
   res.redirect('/register/' + req.params.user);
 });
 
-var generateMenu = function(userID) {
-  var i = 1;
-  var output;
-
-  for(var item in merch.items) {
-    if (merch.items[item].sizes) {
-      output += "<br>" + merch.items[item].name + ":<br>";
-
-      for (size in merch.items[item].sizes) {
-        output += "<input type='radio' name='item' value='{\"item\": \""
-          + item + "\", \"size\": \"" + size + "\"}'>";
-        output += size + ": $" + merch.items[item].sizes[size].toFixed(2) + "<br>";
-      }
-    } else {
-      output += "<br><input type='radio' name='item' value='{\"item\": \"" 
-        + item + "\", \"size\": \"na\"}'>" + merch.items[item].name + ": $"
-        + merch.items[item].amt.toFixed(2) + "<br>";
-    }
-  }
-  return output;
-}
-
-var formatTotals = function(userID) {
-  formattedTotals = "Subtotal: " + database[userID].subTotal.toFixed(2) + "<br>";
-  formattedTotals += "Tax: " + database[userID].tax.toFixed(2) + " @ " + (reg.register.taxRate*100)  + "%<br>";
-  formattedTotals += "Total: " + database[userID].total.toFixed(2);
-
-  console.log("Subtotal: " + database[userID].subTotal.toFixed(2));
-  console.log("Tax: " + database[userID].tax.toFixed(2) + " @ " + (reg.register.taxRate*100)  + "%");
-  console.log("Total: " + database[userID].total.toFixed(2));
-
-  return formattedTotals;
-}
-
 var fetchDB = function() {
   console.log("Initializing database.");
 
@@ -178,8 +111,6 @@ var fetchDB = function() {
         database = JSON.parse(data);
 
         console.log("Database fetched.\n");
-
-        console.log(database);
       });
     } else {
       console.log("Database doesn't exist! Creating..");
@@ -193,8 +124,6 @@ var fetchDB = function() {
           database = JSON.parse(data);
 
           console.log("Database fetched.\n");
-
-          console.log(database);
         });
       });
     }
